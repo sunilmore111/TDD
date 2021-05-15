117bb1d29295e32357b700aabcd7e1d616980fc8
sunilmore111 <sunilmore11197@gmail.com>
2021-05-15 15:50:02 +0530

    Support different delimiters expression //;\n1;2

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.sound.sampled.Line;
 public class StringCalculator {
-	
+
 	public int add(String numbers) {
 		int sum=0;
 		//If String is empty
@@ -12,31 +13,24 @@ public class StringCalculator {
 			return 0;
 		}
 
-		//Handle new lines \n and , in input
-		String [] num=numbers.split("\\\\n|,");
+		//Regex to replace all character except numbers in a string
+		String nums = numbers.replaceAll("[^0-9]+", " ");
+		List<String> num = Arrays.asList(nums.trim().split(" "));
+		for(int i=0;i<num.size();i++) {
+			sum=sum+Integer.parseInt(num.get(i));
+        }
 
-		//Output for one number only
-		if(num.length==1) {
-			return Integer.parseInt(num[0]);
-		}
-	
-		//Output for unknown amount of numbers
-		for(int i=0;i<num.length;i++) {
-			sum=sum+Integer.parseInt(num[i]);
-		}
-		
 		return sum;
 	}
-	
-public static void main(String args[]) throws IOException {
-	
-      BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
-      String numbers = bufferReader.readLine();
-      
-	  StringCalculator sc=new StringCalculator();
 
-	  System.out.println(sc.add(numbers));
-	  
+	public static void main(String args[]) throws IOException {
+
+		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+		String numbers = bufferReader.readLine();
+
+		StringCalculator sc = new StringCalculator();
+
+		System.out.println(sc.add(numbers));
+
+	}
 }
-}
- 
