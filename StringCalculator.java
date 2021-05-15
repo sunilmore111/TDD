62bf139690149c0a4c7d7cc25caac11a1ad0ca29
sunilmore111 <sunilmore11197@gmail.com>
2021-05-15 17:09:28 +0530

    Throw Exception negatives not allowed

--- StringCalculator.java
+++ StringCalculator.java
@@ -6,20 +6,30 @@ import java.util.List;
 
 public class StringCalculator {
 
-	public int add(String numbers) {
-		int sum=0;
-		//If String is empty
-		if(numbers.isEmpty()) {
+	public int add(String numbers) throws IOException {
+		int sum = 0;
+		// If String is empty
+		if (numbers.isEmpty()) {
 			return 0;
 		}
 
-		//Regex to replace all character except numbers in a string
-		String nums = numbers.replaceAll("[^0-9]+", " ");
+		// Regex to replace all characters except positive and negative numbers in a string
+		String nums = numbers.replaceAll("[^-?0-9]+", " ");
 		List<String> num = Arrays.asList(nums.trim().split(" "));
-		for(int i=0;i<num.size();i++) {
-			sum=sum+Integer.parseInt(num.get(i));
-        }
+		try {
+			for (int i = 0; i < num.size(); i++) {
 
+				// Throw Exception if negative number found and return sum 0
+				if(Integer.parseInt(num.get(i)) < 0) {
+					throw new Exception("negatives not allowed " + " Negative number passed: "+Integer.parseInt(num.get(i)));
+				}else {
+					sum = sum + Integer.parseInt(num.get(i));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
 		return sum;
 	}
 
