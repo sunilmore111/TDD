6e36d6327a8a7de66689b53d40f6b83294d32deb
sunilmore111 <sunilmore11197@gmail.com>
2021-05-16 02:36:39 +0530

    Method to handle add() trigger and Invalid input test

--- StringCalculator.java
+++ StringCalculator.java
@@ -4,21 +4,22 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
-public class StringCalculator {
+public class StringCalculator implements AddListener {
 
 	static int count = 0, sum = 0;
-	
+
 	// Method to count how many times add () was called
 	public int getCalledCount() {
 		count++;
 		return count;
 	}
-	
+
 	public int add(String numbers) throws IOException {
-		
+
 		getCalledCount();
-		
+
 		// If String is empty
 		if (numbers.isEmpty()) {
 			return sum;
@@ -29,7 +30,7 @@ public class StringCalculator {
 		List<String> num = Arrays.asList(nums.trim().split(" "));
 		ArrayList<Integer> negative = new ArrayList<>();
 		try {
-			
+
 			for (int i = 0; i < num.size(); i++) {
 
 				// Storing all negative numbers to pass in Exception
@@ -37,16 +38,16 @@ public class StringCalculator {
 					negative.add(Integer.parseInt(num.get(i)));
 				}
 			}	
-			
+
 			// Handling multiple negative numbers
 			for (int i = 0; i < num.size(); i++) {
-				
+
 				if(negative.size() > 0) {
 					throw new Exception("negatives not allowed " + " Negative number passed: "+Arrays.asList(negative));
 				} else {
 					sum = sum + Integer.parseInt(num.get(i));
 				}
-				
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -55,25 +56,45 @@ public class StringCalculator {
 		return sum;
 	}
 
+	// Method for add() trigger
+	@Override
+	public void addOccured(String numbers) {
+
+		Pattern pattern = Pattern.compile("[//]*[-?0-9]*(\\\\n)?([,*;]?(\\\\n)?[-?0-9]+)+");
+
+		try {
+
+			if(pattern.matcher(numbers).matches()) {
+				add(numbers);
+				System.out.println("Add() Triggered");
+			}
+			else {
+				System.out.println("Add() was not Triggered ");
+			} 
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String args[]) throws IOException {
 
 		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
 		String numbers = bufferReader.readLine();
 
 		StringCalculator sc = new StringCalculator();
-		
 
 		if(numbers.isEmpty()) { 
-			sc.add(numbers); 
-		}else {
 			sc.add(numbers);
+		}else {
+			sc.addOccured(numbers);
 			while(!(numbers=bufferReader.readLine()).isEmpty()) {	
-				sc.add(numbers);
+				sc.addOccured(numbers);
 			}
 		}
- 
+
 		System.out.println(sum);
-		
+
 		System.out.println("The Add() was called "+ count +" times");
 	}
 }
